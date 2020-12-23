a9c7d8d58e068db97eba285f6a37b2b20f3ad649
Pablolo <pablo_olondriz@hotmail.com>
2020-12-23 19:48:38 +0100

    installing packages

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      // 'serif': ['Georgia', 'Cambria', ...],
+      // 'mono': ['SFMono-Regular', 'Menlo', ...],
+      sans: ['Nunito', 'sans-serif'],
+    },
+    fontSize: {
+      'super-xs': '.65rem',
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+  },
+  variants: { borderColor: ['responsive', 'hover', 'focus', 'focus-within'] },
+  plugins: [],
+}
