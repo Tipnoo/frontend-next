985e01790ccf1a6b175539e78f89a430f492ae34
Pablolo <pablo_olondriz@hotmail.com>
2021-03-24 19:59:09 +0000

    fixed function accordion to accept jsx

--- components/content/privacyPageContent.js
+++ components/content/privacyPageContent.js
@@ -1,3 +1,15 @@
-export const webOwner = 'fuck';
+export const webOwner = (
+  <div>
+    <p>hola</p>
+  </div>
+);
 
 export const personalData = 'fuck twice';
+
+export const informationUsage = 'fuck twice';
+
+export const cookiePolicy = 'fuck twice';
+
+export const legalResponsibility = 'fuck twice';
+
+export const expectUsers = 'fuck twice';
--- pages/privacy-cookies.js
+++ pages/privacy-cookies.js
@@ -5,40 +5,29 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Footer from '@components/Footer';
 import HeaderPostAnOffer from '@components/headers/Header-postAnOffer';
-import { webOwner, personalData } from '@components/content/privacyPageContent';
+import {
+  webOwner, personalData, informationUsage, cookiePolicy, legalResponsibility, expectUsers,
+} from '@components/content/privacyPageContent';
 
 const PrivacyCookies = () => {
-  const eachDropdown = () => {
-    const privacyTitles = [
-      'Web Owner',
-      'Protection of Personal Data',
-      'Collection & Usage of Information',
-      'Cookie Policy',
-      'Legal Responsibility for Content',
-      'What we Expect from our Users',
-    ];
-    return privacyTitles.map((eachTitle, index) => (
-      <div key={index}>
-        <Accordion className="mt-4 mb-2 mx-4">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className="">{eachTitle}</Typography>
-          </AccordionSummary>
-          <AccordionDetails className="bg-blue-50">
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-              sit amet blandit leo lobortis eget. 
-              {webOwner} 
-              {personalData}
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-    ));
-  };
+  const dropdown = (title, content) => (
+    <div>
+      <Accordion className="mt-4 mb-2 mx-4">
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className="">{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails className="bg-blue-50">
+          <Typography component="span">
+            {content}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 
   return (
     <div>
@@ -47,7 +36,12 @@ const PrivacyCookies = () => {
         subtitle="and Cookie Settings"
       />
       <div className="bg-gray-100 text-center rounded-b text-teal-darkest px-2 py-3 shadow-md h-screen">
-        {eachDropdown()}
+        {dropdown('Web Owner', webOwner)}
+        {dropdown('Protection of Personal Data', personalData)}
+        {dropdown('Collection & Usage of Information', informationUsage)}
+        {dropdown('Cookie Policy', cookiePolicy)}
+        {dropdown('Legal Responsibility for Content', legalResponsibility)}
+        {dropdown('What we Expect from our Users', expectUsers)}
       </div>
       <Footer />
     </div>
