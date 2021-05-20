46caf010e3374f1be98ce3b95f8a010e8766a6b0
Pablolo <pablo_olondriz@hotmail.com>
2021-05-20 14:14:10 +0100

    some css changes

--- components/buttons/Btn-returnToHome.js
+++ components/buttons/Btn-returnToHome.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 const ButtonReturnToHome = () => (
-  <div className="mt-8 text-lg bg-yellow-600 hover:bg-yellow-500 transform hover:-translate-y-0.5 text-white font-bold py-4 px-4 rounded w-4/5 md:w-3/5 border-b-4 border-r-2 border-yellow-700 mb-4 mx-auto cursor-pointer">
+  <div className="mt-8 text-lg bg-yellow-600 hover:bg-yellow-500 transform hover:translate-y-0.5 text-white font-bold py-4 px-4 rounded w-4/5 md:w-3/5 lg:w-2/5 border-b-4 border-r-2 border-yellow-700 mb-4 mx-auto cursor-pointer">
     <Link
       href="/"
     >
