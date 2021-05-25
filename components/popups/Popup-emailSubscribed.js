a79396ba88a975330280fe861b4c8ae24c60a377
Pablolo <pablo_olondriz@hotmail.com>
2021-05-25 20:25:18 +0100

    fixed all popups

--- components/popups/Popup-emailSubscribed.js
+++ components/popups/Popup-emailSubscribed.js
@@ -9,39 +9,37 @@ const PopupEmailSubscribed = (props) => {
   };
 
   return (
-    <div className="fixed z-50 w-screen h-80 top-0 text-center mx-auto my-auto">
-      <div className="bg-indigo-500 rounded-md w-11/12 md:w-9/12 md:px-24 shadow-2xl h-full mx-auto mt-48 flex-row relative">
-        <div className="absolute top-0 right-6">
-          <span
-            className="text-white text-4xl font-extrabold cursor-pointer"
-            onClick={closePopup}
-            role="button"
-            tabIndex={0}
-          >
-            &times;
-          </span>
-        </div>
-        <img
-          className="h-16 md:h-24 mx-auto pt-4"
-          src="/images/email-love.png"
-          alt="email"
-        />
-        <h3 className="font-bold text-lg mt-4 w-4/5 mx-auto text-white">
-          Thank you! We've sent you an email to confirm your subscription!
-        </h3>
-        <p className="w-4/5 mt-4 mb-2 mx-auto text-white">
-          After you confirm, we will start sending you your email alerts.
-        </p>
-        <div>
-          <button
-            onClick={closePopup}
-            className="bg-green-500 hover:bg-green-700 text-white border-b-4 border-r-2 border-green-600 font-bold py-2 px-4 rounded w-2/5 mt-4 mb-4 mx-auto cursor-pointer"
-            role="button"
-            tabIndex={0}
-          >
-            Ok, got it!
-          </button>
-        </div>
+    <div className="fixed z-50 top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center bg-indigo-500 rounded-md w-11/12 md:w-8/12 xl:w-5/12 mx-auto flex-row shadow-2xl">
+      <div className="absolute top-0 right-6">
+        <span
+          className="text-white text-4xl font-extrabold cursor-pointer"
+          onClick={closePopup}
+          role="button"
+          tabIndex={0}
+        >
+          &times;
+        </span>
+      </div>
+      <img
+        className="h-16 md:h-24 mx-auto pt-4"
+        src="/images/email-love.png"
+        alt="email"
+      />
+      <h3 className="font-bold text-lg xl:text-xl mt-4 w-4/5 mx-auto text-white">
+        Thank you! We've sent you an email to confirm your subscription!
+      </h3>
+      <p className="w-4/5 mt-4 mb-2 mx-auto text-white">
+        After you confirm, we'll start sending you your email alerts.
+      </p>
+      <div>
+        <button
+          onClick={closePopup}
+          className="bg-green-500 hover:bg-green-700 text-white border-b-4 border-r-2 border-green-600 font-bold py-2 px-4 rounded w-2/5 mt-4 mb-4 lg:mb-8 mx-auto cursor-pointer"
+          role="button"
+          tabIndex={0}
+        >
+          Ok, got it!
+        </button>
       </div>
     </div>
   );
