d2b6b57ac89cfea3356ea85417c36716ecec0b3e
Pablolo <pablo_olondriz@hotmail.com>
2021-07-22 16:53:19 +0200

    modyfing next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
     BACKEND_URI: process.env.BACKEND_URI,
     STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://tipnoo.herokuapp.com/:path*',
+      },
+    ];
+  },
 };
